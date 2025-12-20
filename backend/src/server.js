606397963768c21988ca4e649551e4a2065d8b38
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-12-20 12:42:09 +0530

    feat: remove unused environment variables and controllers, add error handling utility and response classes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
-import app from './app.js'
-import dbConnect from './config/dbConnect.js';
+import app from "./app.js";
+import dbConnect from "./config/dbConnect.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-
-dbConnect().then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port: ${process.env.PORT}`);
+dbConnect()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed", error);
   });
-}).catch((error)=>{
-    console.log("MONGODB connection failed", error)
-})
